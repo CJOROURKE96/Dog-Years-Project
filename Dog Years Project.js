bbac2e0316ed5d32e81fd49851f466832d5b314c
CJOROURKE96 <101739565+CJOROURKE96@users.noreply.github.com>
2022-07-03 21:34:48 +0100

    Add files via upload

---
+++ Dog Years Project.js
@@ -0,0 +1,16 @@
+// My current age //
+const myAge = 26
+// Early dog years // 
+let earlyYears = 2
+earlyYears *= 10.5
+// Later dog years //
+let laterYears = myAge - 2
+// Accounting for later years //
+laterYears *= 4
+console.log(`${earlyYears} is the value of the early years`)
+console.log(`${laterYears} is the value of the later years`)
+// This is my total age in dog years //
+const myAgeInDogYears = earlyYears + laterYears
+// My name in lowercase // 
+const myName = 'Connor'.toLowerCase()
+console.log(`My name is ${myName}. I am a ${myAge} year old in human years which is ${myAgeInDogYears} years old in dog years.`)
